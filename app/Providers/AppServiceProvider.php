eb196e6cb8aae587151f0f60f59a10ac7c2438f0
Revaldo Nyasha <nyasharevaldo@gmail.com>
2026-02-28 22:41:51 +0200

    fix2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,11 +3,16 @@
 namespace App\Providers;
 
 use Carbon\CarbonImmutable;
+use Google\Cloud\Storage\StorageClient;
+use Illuminate\Filesystem\FilesystemAdapter;
 use Illuminate\Support\Facades\Date;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Validation\Rules\Password;
+use League\Flysystem\Filesystem;
+use League\Flysystem\GoogleCloudStorage\GoogleCloudStorageAdapter;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -29,6 +34,23 @@ class AppServiceProvider extends ServiceProvider
         }
 
         $this->configureDefaults();
+
+        Storage::extend('gcs', function ($app, array $config) {
+            $client = new StorageClient([
+                'projectId' => $config['project_id'],
+                'keyFilePath' => $config['key_file'] ?? null,
+            ]);
+
+            $bucket = $client->bucket($config['bucket']);
+
+            $adapter = new GoogleCloudStorageAdapter($bucket, $config['root'] ?? '');
+
+            return new FilesystemAdapter(
+                new Filesystem($adapter, $config),
+                $adapter,
+                $config
+            );
+        });
     }
 
     protected function configureDefaults(): void
